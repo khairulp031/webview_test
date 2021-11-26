6d9d7c003d0a7a125a91c3b50bac7d1243a4f364
khairulp031 <90588995+khairulp031@users.noreply.github.com>
2021-11-27 01:27:42 +0800

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const isWebview = (navigator.userAgent && navigator.userAgent.toLowerCase().includes('webview'))
+
+declare global {
+  interface Window {
+    testInit: Function
+  }
+}
+
 function App() {
+  const [text, setText] = React.useState('')
+  const [data, setData] = React.useState({})
+  const [iframUrl, setIframeUrl] = React.useState<string | undefined>()
+  const iframeRef = React.useRef<HTMLIFrameElement>(null)
+
+
+  React.useEffect(() => {
+    if (isWebview) {
+      setIframeUrl('abc://window.testInit?query=' + encodeURI('{}'))
+    }
+  }, [])
+
+  React.useEffect(() => {
+    if (iframUrl && iframeRef.current) {
+      window.testInit = function (input: string) {
+        setText(input)
+        try {
+          setData(JSON.parse(input))
+        } catch (e) {
+          console.log(e)
+        }
+        return 'good'
+      }
+      iframeRef.current.src = iframUrl
+    }
+  }, [iframUrl])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div><strong>URL</strong>: {document.location.href}</div>
+      <div><strong>isWebview</strong>: {isWebview ? 'true' : 'false'}</div>
+      <div><strong>UserAgent</strong>: {navigator.userAgent}</div>
+      <div><strong>text from Native</strong>: {text}</div>
+      <div><strong>data in JSON</strong>: {JSON.stringify(data)}</div>
+      {
+        isWebview &&
+        <iframe
+          title="webview"
+          ref={iframeRef}
+          width='100%' height='100vh' src=''
+          style={{ visibility: 'hidden' }}
+        />
+      }
+    </>
   );
 }
 
